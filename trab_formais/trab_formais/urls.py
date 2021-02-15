99a9d339104fb9bd4ba881c1e9ed5bc074a08512
beringela <mateusmanoel2009@hotmail.com>
2021-02-15 15:31:24 -0300

    first view

--- trab_formais/trab_formais/urls.py
+++ trab_formais/trab_formais/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
     path('io_utils/', include('io_utils.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
