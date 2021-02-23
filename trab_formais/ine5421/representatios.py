ff1a70838561cc075fb5390687d6c4c2aa61719b
Arthur Alves <artmr.alves@gmail.com>
2021-02-23 00:34:26 -0300

    Tratamento de erros iniciados, porém alguns ainda precisam ser implementados; atualização dos endpoints de upload e atualização de gr e af.

--- trab_formais/ine5421/representatios.py
+++ trab_formais/ine5421/representatios.py
@@ -1,36 +1,109 @@
 class GR:
+    """
+    Classe usada para representar uma Gramática Regular
+
+    Attributes
+    ----------
+    start_symbol: str
+        Símbolo inicial.
+    non_terminals: list
+        Lista contendo os símbolos não terminais da gramática.
+    terminals: list
+        Lista contendo os símbolos terminais da gramática.
+    productions: dict
+        Dicionário de listas, no qual cada chave é uma sentença válida da GR e lista associada o corpo da produção.
+    production_heads: list
+        Lista contendo todas as chaves de 'productions'.
+
+    Methods
+    -------
+    write_to_file(str=filename)
+        Escreve o objeto em um arquivo de nome 'filename'.
+    string_in_file_format()
+        Retorna o objeto codificado em uma string seguindo o formato de arquivo '.jff'.
+
+
+    """
+    ERRO_1 = "Deve haver apenas um símbolo inicial.(linha 1)"
+    ERRO_2_1 = "O símbolo inicial \""
+    ERRO_2_2 = "\" não pertence ao conjunto de não terminais.(linha 2)"
+    ERRO_3_1 = "O símbolo \""
+    ERRO_3_2 = "\" não pode pertencer aos conjuntos de terminais e não-terminais simultaneamente.(linhas 2 e 3)"
+    ERRO_4 = "Todas as produções devem separar a cabeça e corpo da produção com \"->\".(linha "
+    # TODO: talvez esse caso nunca ocorra
+    ERRO_5 = "As cabeças de produção não podem ser nulas.(linha "
+    ERRO_6 = "Não pode haver encolhimento da sentença.(linha "
+    ERRO_7 = "Todas as produções de uma \"cabeça de produção\" devem ser definidas em apenas uma linha. (linha "
+
     def __init__(self, meta_data, productions):
-        # checks the presence of an start_symbol
-        if str(meta_data[0]) == "":
-            raise Exception("There is no start symbol.")
+        """
+        :param meta_data: list
+            lista contendo, NA SEGUINTE ORDEM: símbolo inicial, símbolos não terminais, símbolos terminais.
+        :param productions: list
+            lista contendo as regras de produção da GR.
+        :raise Exception:
+            erro referente a falha no processo de leitura das informações da GR
+        """
+        # verifica se há apenas um símbolo inicial
+        if str(meta_data[0]) == "" or len([str(nonTerminal) for nonTerminal in meta_data[0].split(",")]) > 1:
+            raise Exception(self.ERRO_1)
         self.start_symbol = str(meta_data[0])
+        # verifica se há pelo menos um símbolo
         self.non_terminals = [str(symbol) for symbol in meta_data[1].split(",")]
-        # checks if the start_symbol belongs to the non_terminals
         if self.start_symbol not in self.non_terminals:
-            raise Exception("Start symbol doesn't belong to non teminals.")
+            raise Exception(self.ERRO_2_1 + self.start_symbol + self.ERRO_2_2)
+        # verifica se nenhum dos símbolos não terminais pertence ao conjunto de terminais
         self.terminals = [str(symbol) for symbol in meta_data[2].split(",")]
-        # create the productions
+        for symbol in self.non_terminals:
+            if symbol in self.terminals:
+                raise Exception(self.ERRO_3_1 + symbol + self.ERRO_3_2)
+        # cria as produções
         self.productions = dict()
         for production in productions:
-            head, body = [str(p) for p in production.split("->")]
+            try:
+                head, body = [str(p) for p in production.split("->")]
+                if head == "":
+                    line = str(4 + productions.index(production))
+                    raise Exception(self.ERRO_5 + line + ")")
+            except:
+                line = str(4 + productions.index(production))
+                raise Exception(self.ERRO_4 + line + ")")
+            # TODO: implementar a detecção de encolhimento de sentença
             body = [str(b) for b in body.split("|")]
+            if len(body) < 1 or body[0] == "":
+                line = str(4 + productions.index(production))
+                raise Exception(self.ERRO_6 + line + ")")
             if head in self.productions.keys():
-                raise Exception("Must be only one line for each 'production head'.")
+                line = str(4 + productions.index(production))
+                raise Exception(self.ERRO_7 + line + ")")
+            # adiciona essa regra de produção ao dicionário de produções
             self.productions.update({head: body})
         self.production_heads = list(self.productions.keys())
 
     def __str__(self):
-        return f"Start symbol: {self.start_symbol}\n" \
-               f"Non terminals: {self.non_terminals}\n" \
-               f"Terminals: {self.terminals}\n" \
-               f"Productions: {self.productions}\n" \
-               f"Production heads: {self.production_heads}"
+        """
+        :return: str com a representação dos dados do objeto; NÃO segue o padrão dos arquivos '.jff'.
+        """
+        return f"Símbolo inicial: {self.start_symbol}\n" \
+               f"Não terminais: {self.non_terminals}\n" \
+               f"Terminais: {self.terminals}\n" \
+               f"Produções: {self.productions}\n" \
+               f"Cabeças de produção: {self.production_heads}"
 
     def write_to_file(self, filename):
+        """
+        :param filename: str
+            nome do arquivo no qual a gr deve ser escrita.
+        :return:
+        """
         with open(filename, "w") as file:
             file.write(self.string_in_file_format())
 
     def string_in_file_format(self):
+        """
+        :return: str
+            string representando o formato a GR seguindo o formato dos arquivos '.jff'.
+        """
         productions = ""
         # format the production dict entries to the file format
         for head, body in self.productions.items():
@@ -43,15 +116,52 @@ class GR:
 
 
 class AF:
-    ERRO_1 = "O número de estados(linha 1) deve ser inteiro e maior que 0."
-    ERRO_2 = "Não há estado inicial(linha 2)."
-    ERRO_3 = "Não pode haver mais de um estado inicial(linha 2)."
+    """
+        Classe usada para representar um Autômato Finito
+
+        Attributes
+        ----------
+        n_states: int
+            Número de estados.
+        start_state: str
+            Estado inicial.
+        accept_states: list
+            Lista contendo os estados de aceitação.
+        alphabet: list
+            Lista contendo os símbolos do alfabeto.
+        transition_table: dict
+            Dicionário de dicionários, no qual cada par chave-valor emula uma linha na tabela de transições.
+        is_AFND: bool
+            Diz se o AF é não determinístico.
+        states: list
+            Lista contendo os nomes de todos os estados do AF.
+
+        Methods
+        -------
+        write_to_file(str=filename)
+            Escreve o objeto em um arquivo de nome 'filename'.
+        string_in_file_format()
+            Retorna o objeto codificado em uma string seguindo o formato de arquivo '.jff'.
+
+
+        """
+    ERRO_1 = "O número de estados deve ser inteiro e maior que 0.(linha 1)"
+    ERRO_2 = "Não há estado inicial.(linha 2)"
+    ERRO_3 = "Não pode haver mais de um estado inicial.(linha 2)"
     ERRO_4 = "Por questões de legibilidade, nomes de estados não podem ser símbolos do alfabeto.(linha "
     ERRO_5 = "Se presentes, as linhas de transição devem conter 3 elementos separados por \",\".(linha "
     ERRO_6_1 = "O símbolo \""
     ERRO_6_2 = "\" não pertence ao alfabeto definido.(linha "
 
     def __init__(self, meta_data, transitions):
+        """
+        :param meta_data: list
+            lista contendo, NA SEGUINTE ORDEM: número de estados, estado inicial, estados de aceitação, alfabeto.
+        :param transitions: list
+            tabela de transição.
+        :raise Exception:
+            erro referente a falha no processo de leitura das informações do AF
+        """
         # lê o numero de estados
         try:
             self.n_states = int(meta_data[0])
@@ -116,6 +226,9 @@ class AF:
         self.n_states = len(self.states)
 
     def __str__(self):
+        """
+        :return: str com a representação dos dados do objeto; NÃO segue o padrão dos arquivos '.jff'.
+        """
         return f"Número de estados: {self.n_states}\n" \
                f"Estado Inicial: {self.start_state}\n" \
                f"Estados de aceitação: {self.accept_states}\n" \
@@ -124,10 +237,19 @@ class AF:
                f"Nomes dos estados: {self.states}"
 
     def write_to_file(self, filename):
+        """
+        :param filename: str
+            nome do arquivo no qual o af deve ser escrito.
+        :return:
+        """
         with open(filename, "w") as file:
             file.write(self.string_in_file_format())
 
     def string_in_file_format(self):
+        """
+        :return: str
+            string representando o formato o AF seguindo o formato dos arquivos '.jff'.
+        """
         transition_table_as_string = ""
         # Formata as entradas do dicionário para o formato do arquivo .jff
         for origin_state, transitions in self.transition_table.items():
--- trab_formais/io_utils/views.py
+++ trab_formais/io_utils/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.conf import settings
 
 from .forms import InputForm
-from ine5421.functions import read_af, read_gr
+from ine5421.functions import read_af_file, read_gr_file, read_af_string, read_gr_string
 
 FILENAME_AF = settings.MEDIA_ROOT + '/af_file'
 FILENAME_ER = settings.MEDIA_ROOT + '/er_file'
@@ -31,7 +31,8 @@ def finite_automata(request):
 
 
 def update_or_upload_af(request):
-    # talvez não seja a melhor prática, mas por enquanto permite
+    # talvez não seja a melhor prática, mas por enquanto permite ter acesso a todos os campos do formulário nas
+    # duas funções
     if request.POST['option'] == 'Ler arquivo':
         return upload_af_file(request)
     elif request.POST['option'] == 'Atualizar AF':
@@ -39,32 +40,35 @@ def update_or_upload_af(request):
 
 
 def update_af_file(request):
+    # TODO:impedir que as linhas vazias sejam excluídas
+    context = dict()
     try:
         file_content = request.POST['file_content']
     except:
-        context = {'error': 'Não há nada para editar.',
-                   'form': InputForm(), }
+        context.update({'error1': 'Não há nada para editar.',
+                        'form': InputForm()})
         return render(request, 'af.html', context)
-    filename = settings.MEDIA_ROOT + '/af_file'
-    with open(filename, 'w') as fout:
-        print(file_content, file=fout)
-    context = {
-        'file_content': file_content,
-    }
+    if 'error1' not in context.keys():
+        filename = settings.MEDIA_ROOT + '/af_file'
+        try:
+            af = read_af_string(file_content)
+            with open(filename, 'w') as fout:
+                print(file_content, file=fout)
+        except Exception as e:
+            context.update({'error1': e})
+        finally:
+            context.update({'file_content': file_content})
     return render(request, 'af.html', context)
 
 
 def upload_af_file(request):
+    # TODO:impedir que as linhas vazias sejam excluídas
     context = dict()
     try:
         uploaded_file = request.FILES['afFile']
     except:
-        context.update({'error': 'Você não selecionou um arquivo.'})
-        if 'file_content' in request.POST.keys():
-            context.update({'file_content': request.POST['file_content']})
-        else:
-            context.update({'form': InputForm()})
-    if 'error' not in context.keys():
+        context.update({'error1': 'Você não selecionou um arquivo.'})
+    if 'error1' not in context.keys():
         # writing file for temp use
         output_file = open(FILENAME_AF, 'w')
         # Check size of file, only open if its not too big
@@ -76,24 +80,31 @@ def upload_af_file(request):
         output_file.close()
 
         try:
-            af = read_af(FILENAME_AF)
+            af = read_af_file(FILENAME_AF)
         except Exception as e:
-            context.update({'error': e})
+            context.update({'error1': e})
 
-    if 'error' not in context.keys():
+    if 'error1' not in context.keys():
         context.update({'file_content': file_content})
     else:
         if 'file_content' in request.POST.keys():
-            context.update({'file_content': request.POST['file_content']})
+            af_string = request.POST['file_content']
+            try:
+                af = read_af_string(af_string)
+                context.update({'file_content': af_string})
+            except Exception as e:
+                context.update({'error2': e})
+                context.update({'file_content': af_string})
         else:
             context.update({'form': InputForm()})
     return render(request, 'af.html', context)
 
 
 def download_af_file(request):
+    # TODO: talvez checar se a estrutura é válida antes de permitir o download
     response = HttpResponse(open(FILENAME_AF, 'rb').read())
     response['Content-Type'] = 'text/plain'
-    response['Content-Disposition'] = 'attachment; filename=AF.txt'
+    response['Content-Disposition'] = 'attachment; filename=AF.jff'
     return response
 
 
@@ -110,6 +121,8 @@ def regular_grammar(request):
 
 
 def update_or_upload_gr(request):
+    # talvez não seja a melhor prática, mas por enquanto permite ter acesso a todos os campos do formulário nas
+    # duas funções
     if request.POST['option'] == 'Ler arquivo':
         return upload_gr_file(request)
     elif request.POST['option'] == 'Atualizar GR':
@@ -117,53 +130,68 @@ def update_or_upload_gr(request):
 
 
 def update_gr_file(request):
+    # TODO:impedir que as linhas vazias sejam excluídas
+    context = dict()
     try:
         file_content = request.POST['file_content']
     except:
-        context = {'error': 'Não há nada para editar.',
-                   'form': InputForm(), }
+        context.update({'error1': 'Não há nada para editar.',
+                        'form': InputForm()})
         return render(request, 'gr.html', context)
-    filename = settings.MEDIA_ROOT + '/gr_file'
-    with open(filename, 'w') as fout:
-        print(file_content, file=fout)
-    context = {
-        'file_content': file_content,
-    }
+    if 'error1' not in context.keys():
+        filename = settings.MEDIA_ROOT + '/gr_file'
+        try:
+            gr = read_gr_string(file_content)
+            with open(filename, 'w') as fout:
+                print(file_content, file=fout)
+        except Exception as e:
+            context.update({'error1': e})
+        finally:
+            context.update({'file_content': file_content})
     return render(request, 'gr.html', context)
 
 
 def upload_gr_file(request):
+    # TODO:impedir que as linhas vazias sejam excluídas
+    context = dict()
     try:
         uploaded_file = request.FILES['grFile']
     except:
-        context = {'error': 'Você não selecionou um arquivo'}
+        context.update({'error1': 'Você não selecionou um arquivo.'})
+    if 'error1' not in context.keys():
+        # writing file for temp use
+        output_file = open(FILENAME_AF, 'w')
+        # Check size of file, only open if its not too big
+        if not uploaded_file.multiple_chunks():
+            file_content = str(uploaded_file.read(), 'utf-8')
+            output_file.write(file_content)
+        else:
+            print('File too big')
+        output_file.close()
+
+        try:
+            gr = read_gr_file(FILENAME_AF)
+        except Exception as e:
+            context.update({'error1': e})
+
+    if 'error1' not in context.keys():
+        context.update({'file_content': file_content})
+    else:
         if 'file_content' in request.POST.keys():
-            context.update({'file_content': request.POST['file_content']})
+            gr_string = request.POST['file_content']
+            try:
+                gr = read_gr_string(gr_string)
+                context.update({'file_content': gr_string})
+            except Exception as e:
+                context.update({'error2': e})
+                context.update({'file_content': gr_string})
         else:
             context.update({'form': InputForm()})
-        return render(request, 'gr.html', context)
-    # writing file for temp use
-    output_file = open(FILENAME_GR, 'w')
-    # Check size of file, only open if its not too big
-    if not uploaded_file.multiple_chunks():
-        file_content = str(uploaded_file.read(), 'utf-8')
-        output_file.write(file_content)
-    else:
-        print('File too big')
-    output_file.close()
-
-    try:
-        gr = read_gr(FILENAME_GR)
-    except:
-        context = {'error': ''}
-        
-    context = {
-        'file_content': file_content,
-    }
     return render(request, 'gr.html', context)
 
 
 def download_gr_file(request):
+    # TODO: talvez checar se a estrutura é válida antes de permitir o download
     response = HttpResponse(open(FILENAME_GR, 'rb').read())
     response['Content-Type'] = 'text/plain'
     response['Content-Disposition'] = 'attachment; filename=GR.jff'
@@ -184,7 +212,8 @@ def regular_expression(request):
 
 
 def update_or_upload_er(request):
-    # talvez não seja a melhor prática, mas por enquanto permite
+    # talvez não seja a melhor prática, mas por enquanto permite ter acesso a todos os campos do formulário nas
+    # duas funções
     if request.POST['option'] == 'Ler arquivo':
         return upload_er_file(request)
     elif request.POST['option'] == 'Atualizar ER':
@@ -192,10 +221,12 @@ def update_or_upload_er(request):
 
 
 def update_er_file(request):
+    # TODO:impedir que as linhas vazias sejam excluídas
+    # TODO: implementar detecção de erros no processo de atualização;
     try:
         file_content = request.POST['file_content']
     except:
-        context = {'error': 'Não há nada para editar.',
+        context = {'error1': 'Não há nada para editar.',
                    'form': InputForm(), }
         return render(request, 'er.html', context)
     file_content = request.POST['file_content']
@@ -209,10 +240,12 @@ def update_er_file(request):
 
 
 def upload_er_file(request):
+    # TODO:impedir que as linhas vazias sejam excluídas
+    # TODO: criar estrutura para manipulação de ER; implementar detecção de erros no processo de leitura;
     try:
         uploaded_file = request.FILES['erFile']
     except:
-        context = {'error': 'Você não selecionou um arquivo'}
+        context = {'error1': 'Você não selecionou um arquivo'}
         if 'file_content' in request.POST.keys():
             context.update({'file_content': request.POST['file_content']})
         else:
@@ -241,6 +274,7 @@ def regex(request):
 
 
 def download_er_file(request):
+    # TODO: talvez checar se a estrutura é válida antes de permitir o download
     response = HttpResponse(open(FILENAME_ER, 'rb').read())
     response['Content-Type'] = 'text/plain'
     response['Content-Disposition'] = 'attachment; filename=ER.jff'
